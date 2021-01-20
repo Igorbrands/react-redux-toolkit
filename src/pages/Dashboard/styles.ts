f7f54cee79a9e6abdb04bff4cfee5907e8cca5c1
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-01-20 18:36:51 -0400

    feat: create page dashboard

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import colors from "../../styles/colors";
+
+export const Container = styled.div`
+  background-color: ${colors.antiqueWhite};
+  height: 100%; 
+`;
