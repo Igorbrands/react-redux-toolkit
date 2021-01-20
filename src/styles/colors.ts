0d7efb145c210f01c3e9215c33895d40b2203100
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-01-20 18:36:33 -0400

    style: create colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+  mediumPurple: "#9381FF",
+  maximumBluePurple: "#B8B8FF",
+  ghostWhite: "#F8F7FF",
+  antiqueWhite: "#FFEEDD",
+  unbleachedSilk: "#FFD8BE",
+};
+
+export default colors;
