01850fd56b79c2e4556ceff36dd9c67d81bd3179
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-01-28 17:05:12 -0400

    feat: add redux-devtools-extension

--- src/store/ConfigureStore.ts
+++ src/store/ConfigureStore.ts
@@ -2,12 +2,16 @@ import { applyMiddleware, createStore } from "redux";
 import createSagaMiddleware from "redux-saga";
 import createRootReducer from "./modules/rootReducer";
 import createRootSaga from "./modules/rootSaga";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [sagaMiddleware];
 
-const store = createStore(createRootReducer, applyMiddleware(...middleware));
+const store = createStore(
+  createRootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 sagaMiddleware.run(createRootSaga);
 
